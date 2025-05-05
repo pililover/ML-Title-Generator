f58f3bbce9bd26bf1f8ff8bb0a9247dd83ffcc51
namphuonghoang <131260726+namphuonghoang@users.noreply.github.com>
2025-05-05 12:56:20 +0700

    Update preprocessing.py fix losse word

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,12 +2,13 @@ import pandas as pd
 import re
 from underthesea import word_tokenize
 
+import unicodedata
+
 def clean_text(text):
-    if pd.isna(text):
-        return text
-    text = re.sub(r'[\\n\\t\\r]', ' ', text)
-    text = re.sub(r'\s+', ' ', text).strip()
-    return text
+    text = text.replace('\xa0', ' ')  # Thay thế non-breaking space
+    text = unicodedata.normalize("NFC", text)
+    text = re.sub(r'[^\x20-\x7E\u00A0-\u1EF9\u0100-\u017F]', '', text)  # Loại bỏ ký tự không thuộc bảng Unicode mở rộng của tiếng Việt
+    return text.strip()
 
 def segment_text(text):
     return word_tokenize(text, format="text")
